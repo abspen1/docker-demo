68924ce77d7083395777b17c72c11ca7eb0da617
abspen1 <abspencer2097@gmail.com>
2020-08-14 19:35:56 -0700

    spice it up

--- docker-examples/serverdock/hello-server.go
+++ docker-examples/serverdock/hello-server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,29 +15,36 @@ import (
  */
 
 // Article is struct for article JSON
-type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
+// type Article struct {
+// 	Title   string `json:"title"`
+// 	Desc    string `json:"desc"`
+// 	Content string `json:"content"`
+// }
 
 // Articles is a slice of Article
-type Articles []Article
+// type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Ttile", Desc: "Test Description", Content: "Hello World"},
-	}
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+	// articles := Articles{
+	// 	Article{Title: "Test Ttile", Desc: "Test Description", Content: "Hello World"},
+	// }
+	fmt.Println("Endpoint Hit: Are you sure you want ALL articles?")
+	// json.NewEncoder(w).Encode(articles)
 }
 
 func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked!")
+	fmt.Fprintf(w, `<h1>Whoa, Go is neat!</h1>
+	<p>Go is fast!</p>
+	<p>... and simple!</p>
+	<p>You %s even add %s</p>
+	<a href="{{ github.com/abspen1 }}" target="_blank">{{ ME }}</a>
+	`, "can", "<strong>variables</strong>")
+	// fmt.Fprintf(w, "Test POST endpoint worked!")
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+	fmt.Fprintf(w, "Expert web design by Austin Spencer")
+	// fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
 func handleRequests() {
