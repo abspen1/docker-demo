4ac051a96b6b5baf1f3c440781691c0c19938fcc
abspen1 <abspencer2097@gmail.com>
2020-08-14 18:50:52 -0700

    remove logging

--- docker-examples/serverdock/hello-server.go
+++ docker-examples/serverdock/hello-server.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -35,19 +34,6 @@ func main() {
 		Addr:         ":8080",
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
-	}
-
-	// Configure Logging
-	// LogFileLocation := os.Getenv("LOG_FILE_LOCATION")
-	LogFileLocation := "/docker-examples/serverdock/logging/log.txt"
-	if LogFileLocation != "" {
-		log.SetOutput(&lumberjack.Logger{
-			Filename:   LogFileLocation,
-			MaxSize:    500, // megabytes
-			MaxBackups: 3,
-			MaxAge:     28,   //days
-			Compress:   true, // disabled by default
-		})
 	}
 
 	// Start Server
